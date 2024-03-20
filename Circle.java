69b673a894d028543b1ee19d8d2eb5a16b151544
noellericci <nricci2002@gmail.com>
2024-03-20 14:49:37 -0400

    fixing the adius attribute of the Circle class

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    private double radius;
+    public void setRadius(double nr) {
+        if(nr > 0) {
+            radius = nr;
+        }
+    }
+    public double getRadius() {
+        return radius;
+    }
+}
+
